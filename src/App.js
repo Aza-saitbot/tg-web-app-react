9bbcd84ad512c9d7efeb85594835497bbcb1ebfe
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-10-26 08:56:29 +0300

    added hooks useTelegram

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { useEffect } from 'react'
 import './App.css'
+import { useTelegram } from './hooks/useTelegram'
 
-const tg=window.Telegram.WebApp
+
 
 const App = () => {
 
+    const {tg,onToggleButton}=useTelegram()
 
     useEffect(()=>{
         // приложения полностью проинициализировался, его можно отрисовать
         tg.ready()
     },[])
 
-    const onClose = () => {
-      tg.close()
-    }
+
 
     return (
         <div className="App">
-            <button onClick={onClose}>Close</button>
+            <button onClick={onToggleButton}>toggle</button>
         </div>
     )
 }
