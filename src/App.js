b0df4a5f2ce21595e0915c50a02025e47e9f5069
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-10-26 07:58:39 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import './App.css'
+
+const tg=window.Telegram.WebApp
+
+const App = () => {
+
+
+    useEffect(()=>{
+        // приложения полностью проинициализировался, его можно отрисовать
+        tg.ready()
+    },[])
+
+    const onClose = () => {
+      tg.close()
+    }
+
+    return (
+        <div className="App">
+            <button onClick={onClose}>Close</button>
+        </div>
+    )
+}
+
+export default App
