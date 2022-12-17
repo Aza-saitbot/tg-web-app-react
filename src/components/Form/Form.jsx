1b46f286337a1cc56bb2d34afe673b220f6bcdca
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-12-17 19:39:25 +0300

    debug form м1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,47 +3,45 @@ import './Form.css'
 import { useTelegram } from '../../hooks/useTelegram'
 
 const Form = () => {
-    const [country,setCountry]=useState('')
-    const [street,setStreet]=useState('')
-    const [subject,setSubject]=useState('physical')
-    const {tg}=useTelegram()
+    const [country, setCountry] = useState('')
+    const [street, setStreet] = useState('')
+    const [subject, setSubject] = useState('physical')
+    const {tg} = useTelegram()
 
     //useCallback - использую для того что бы, при каждом перерисовке,
     // функция не создавалась новая (сохранить ссылку на функцию)
-    const onSendData = useCallback(()=>{
-        const data={
+    const onSendData = useCallback(() => {
+        const data = {
             country,
             street,
             subject
         }
-        console.log('data',data)
-        console.log('tg.sendData',tg)
         tg.sendData(JSON.stringify(data))
-    },[country,street,subject])
+    }, [country, street, subject])
 
-    useEffect(()=>{
+    useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
-        return ()=> {
+        return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    },[onSendData])
+    }, [onSendData])
 
-    useEffect(()=>{
+    useEffect(() => {
         tg.MainButton.setParams({
-            text:'Отправить данные'
+            text: 'Отправить данные'
         })
-    },[])
+    }, [])
 
-    useEffect(()=>{
-        if (!country || !street){
+    useEffect(() => {
+        if (!country || !street) {
             tg.MainButton.hide()
-        }else {
+        } else {
             tg.MainButton.show()
         }
-    },[country,street])
+    }, [country, street])
 
     const onChangeCountry = (e) => {
-      setCountry(e.target.value)
+        setCountry(e.target.value)
     }
     const onChangeStreet = (e) => {
         setStreet(e.target.value)
@@ -53,7 +51,7 @@ const Form = () => {
     }
 
     return (
-        <div className="form">
+        <div className={"form"}>
             <h3>Введите ваше данные</h3>
             <input value={country} onChange={onChangeCountry} className={'input'} type="text" placeholder={'Страна'}/>
             <input value={street} onChange={onChangeStreet} className={'input'} type="text" placeholder={'Улица'}/>
